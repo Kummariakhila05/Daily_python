5cf6a2ebf90465d41a8a6ee184c0567a8b1bc7d4
Kummariakhila05 <kummariakhila40@gmail.com>
2026-04-28 21:25:30 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,22 @@
+#a='1'
+#b='2'
+#print(a+b)
+
+#a='1'
+#b='2'
+#print(int(a)+int(b)) #explicit
+
+#string='15'
+#num=7
+#string_number=int(string)
+#sum=num+string_number
+#print("the sum of both the numbers is:",sum)
+
+#a=9.8
+#b=7
+#print(a+b) #implicit
+
+a=50
+print(type(a))
+b=12.3
+print(type(b))
