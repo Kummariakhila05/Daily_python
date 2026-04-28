05de93f0848144236faca72e10fe10a54cc3cb83
Kummariakhila05 <kummariakhila40@gmail.com>
2026-04-28 21:05:04 +0530

    Add files via upload

---
+++ 01_variables.py
@@ -0,0 +1,4 @@
+a=1 #integer
+b=5.68 #float
+c="akki" #string
+d=True #boolean
---
+++ operators.py
@@ -0,0 +1,30 @@
+#arithmetic
+a=5
+b=10
+c=a+b
+print(c)
+
+#assignment
+a=4-2
+b=6
+b+=3 #increment value by 3
+print(b)
+
+#comparision
+d=5>4
+print(d)
+
+#logical
+a=True or False
+
+''''#truth table of or
+print("True or False is",True or False)
+print("True or True is",True or False)
+print("False or True is",False or True)
+print("False or False is",False or True)
+
+#truth table for and
+print("True or False is",True or False)
+print("True or True is",True or False)
+print("False or True is",False or True)
+print("False or False is",False or True)''''
