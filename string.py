b58aae9525a47c62ae7707a049ed52ef16413303
Kummariakhila05 <kummariakhila40@gmail.com>
2026-04-28 21:46:09 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+#name="akhila"
+#print(name[0])
+
+#apple='''he said
+#he wants to 
+#eat apple'''
+#print(apple)   #to print string through multiple line
+
+name="akhila"
+print("lets use for loop")
+for character in name:
+    print(character)
+
---
+++ takinguserinput.py
@@ -0,0 +1,10 @@
+#a=input("enter name")
+#print("my name is",a)
+
+#a=input("first num")
+#b=input("second num")
+#print(a+b)
+
+a=int(input("first num"))
+b=int(input("second num"))
+print(a+b)
