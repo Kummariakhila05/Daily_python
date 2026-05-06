58dcf1919fa3818d13f9909512117f52447b0853
Kummariakhila05 <kummariakhila40@gmail.com>
2026-05-06 20:21:32 +0530

    Add files via upload

---
+++ ifelsecondition.py
@@ -0,0 +1,23 @@
+a=int(input("enter age"))
+print("age is",a)
+#condition aoperations
+# >,<.>=,<=,==,!=
+if(a<10):
+    print("children")
+elif(a<50):
+    print("you can do it")
+else:
+    print("eligible")
+print("yay")
+
+num=20
+if(num<0):
+    print("num are negative")
+elif(num>0):
+    if(num<=10):
+        print("num between 1-10")
+    if(num<=20):
+        print("num between 1-20")
+else:
+    print("num is zero")
+
